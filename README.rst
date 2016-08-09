2896bd1614f6f9d91772c72b7f56deb3e5ae24db
Timon Tschanz <timon.tschanz@gmail.com>
2016-08-09 17:48:52 +0200

    initial commit

---
+++ README.rst
@@ -0,0 +1,15 @@
+ftw.referencewidget
+==============
+
+
+- Github: https://github.com/4teamwork/ftw.referencewidget
+- Issues: https://github.com/4teamwork/ftw.referencewidget/issues
+- Continuous integration: https://jenkins.4teamwork.ch/search?q=ftw.referencewidget
+
+
+Copyright
+---------
+
+This package is copyright by `4teamwork <http://www.4teamwork.ch/>`_.
+
+``ftw.referencewidget`` is licensed under GNU General Public License, version 2.
---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.0.dev0'
+maintainer = 'Timon Tschanz'
+
+tests_require = [
+    'plone.app.testing',
+]
+
+setup(name='ftw.referencewidget',
+      version=version,
+      description="A reference browser widget (Maintainer %s)" % maintainer,
+
+      long_description=open("README.rst").read() + "\n" + \
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        ],
+
+      keywords='ftw 4teamwork widget reference browser',
+      author='%s, 4teamwork AG' % maintainer,
+      author_email='mailto:info@4teamwork.ch',
+      maintainer=maintainer,
+      url='https://github.com/4teamwork/ftw.referencewidget',
+
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+          'Plone',
+          'setuptools',
+      ],
+
+      extras_require=dict(
+          tests=tests_require,
+          ),
+      tests_require=tests_require,
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
