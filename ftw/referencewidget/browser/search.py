dcd65d3fcb729aea4779a3650787b4f39ba76cce
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2016-09-07 11:44:23 +0200

    Make searchresult traversable, like the regular listing.

--- ftw/referencewidget/browser/search.py
+++ ftw/referencewidget/browser/search.py
@@ -1,5 +1,6 @@
-from ftw.referencewidget.browser.utils import get_selectable_types
 from ftw.referencewidget.browser.utils import extend_with_batching
+from ftw.referencewidget.browser.utils import get_selectable_types
+from ftw.referencewidget.browser.utils import get_traversal_types
 from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
 import json
@@ -22,15 +23,21 @@ class SearchView(BrowserView):
         results, batch_html = extend_with_batching(self.context, results)
         json_prep = {'batching': batch_html, 'items': []}
 
+        traversel_type = get_traversal_types(self.context)
+
         for item in results:
             contenttype = 'contenttype-' \
                 + item.portal_type.replace('.', '-').lower()
+
+            traversable = item.is_folderish and  \
+                (item.portal_type in traversel_type)
 
             label = '{0} ({1})'.format(item.Title, item.getPath())
             json_prep['items'].append({'title': label,
                                        'path': item.getPath(),
                                        'selectable': True,
-                                       'content-type': contenttype
+                                       'content-type': contenttype,
+                                       'traversable': traversable
                                        })
 
         self.request.RESPONSE.setHeader("Content-type", "application/json")
