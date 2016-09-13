08fb0d4f831744e5c970542b5db9452d2f6a6854
Timon Tschanz <timon.tschanz@gmail.com>
2016-09-13 17:25:15 +0200

    fix typeselection in search

--- ftw/referencewidget/tests/test_search_view.py
+++ ftw/referencewidget/tests/test_search_view.py
@@ -18,7 +18,7 @@ class TestGeneratePathbar(TestCase):
         self.portal = self.layer['portal']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         login(self.portal, TEST_USER_NAME)
-        self.folder = create(Builder('folder').titled("A Folder"))
+        self.folder = create(Builder('folder').titled("Testfolder"))
         self.lower_file = create(Builder('file').within(
             self.folder).titled("Test"))
         self.file = create(Builder('file').titled("Sch\xc3\xbctzenpanzer"))
@@ -32,6 +32,21 @@ class TestGeneratePathbar(TestCase):
         result = view()
         results = json.loads(result)
         items = results['items']
+        self.assertEquals(2, len(items))
+        self.assertEquals("/plone/testfolder", items[1]['path'])
+        self.assertEquals("Testfolder (/plone/testfolder)", items[1]['title'])
+
+        self.assertEquals("/plone/testfolder/test", items[0]['path'])
+        self.assertEquals("Test (/plone/testfolder/test)", items[0]['title'])
+
+    def test_only_correct_types(self):
+        self.widget.override = True
+        self.widget.selectable = ["Folder"]
+        self.widget.request['term'] = 'tes'
+        view = SearchView(self.widget, self.widget.request)
+        result = view()
+        results = json.loads(result)
+        items = results['items']
         self.assertEquals(1, len(items))
-        self.assertEquals("/plone/a-folder/test", items[0]['path'])
-        self.assertEquals("Test (/plone/a-folder/test)", items[0]['title'])
+        self.assertEquals("/plone/testfolder", items[0]['path'])
+        self.assertEquals("Testfolder (/plone/testfolder)", items[0]['title'])
