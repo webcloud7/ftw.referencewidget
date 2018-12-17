388808b2f9110989ec30a3b81538c1440a69c6b8
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2018-12-17 12:12:40 +0100

    Try to use the AT event builder for plone 4.

--- ftw/referencewidget/tests/builders.py
+++ ftw/referencewidget/tests/builders.py
@@ -1,5 +1,6 @@
 from ftw.builder import registry
 from ftw.builder.dexterity import DexterityBuilder
+from ftw.referencewidget import IS_PLONE_5_OR_GREATER
 
 
 class SampleContentBuilder(DexterityBuilder):
@@ -10,8 +11,16 @@ registry.builder_registry.register(
     'refwidget sample content', SampleContentBuilder)
 
 
-class EventsBuilder(DexterityBuilder):
-    portal_type = 'Event'
+if IS_PLONE_5_OR_GREATER:
+
+    class EventsBuilder(DexterityBuilder):
+        portal_type = 'Event'
+
+else:
+    from ftw.builder.archetypes import ArchetypesBuilder
+
+    class EventsBuilder(ArchetypesBuilder):
+        portal_type = 'Event'
 
 
 registry.builder_registry.register('event', EventsBuilder)
