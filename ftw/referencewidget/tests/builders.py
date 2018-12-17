cb914b8438f325fb7f7abf3d6876497707eac241
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2018-12-17 11:57:39 +0100

    Remove AT builder, use DX builder instead.

--- ftw/referencewidget/tests/builders.py
+++ ftw/referencewidget/tests/builders.py
@@ -1,16 +1,17 @@
 from ftw.builder import registry
-from ftw.builder.archetypes import ArchetypesBuilder
 from ftw.builder.dexterity import DexterityBuilder
 
 
 class SampleContentBuilder(DexterityBuilder):
     portal_type = 'SampleContent'
 
+
 registry.builder_registry.register(
     'refwidget sample content', SampleContentBuilder)
 
 
-class EventsBuilder(ArchetypesBuilder):
+class EventsBuilder(DexterityBuilder):
     portal_type = 'Event'
+
 
 registry.builder_registry.register('event', EventsBuilder)
