fc15b1dc4b7e116ebd92621cab89aacd3b47b16c
Tina Gerber <tina.gerber@4teamwork.ch>
2019-11-27 09:08:56 +0100

    Add uninstall profile for Plone 5.

---
+++ ftw/referencewidget/tests/test_uninstall.py
@@ -0,0 +1,11 @@
+from ftw.testing import IS_PLONE_5
+from ftw.testing.genericsetup import GenericSetupUninstallMixin
+from ftw.testing.genericsetup import apply_generic_setup_layer
+from unittest import TestCase
+from unittest import skipUnless
+
+
+@apply_generic_setup_layer
+@skipUnless(IS_PLONE_5, 'Test the uninstall profile for Plone 5')
+class TestGenericSetupUninstall(TestCase, GenericSetupUninstallMixin):
+    package = 'ftw.referencewidget'
